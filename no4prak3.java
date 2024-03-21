37dcfbc2b0d204bcb3eef82049af6b05ae70c935
ANDIALVIANSYAH <147214020+ANDIALVIANSYAH@users.noreply.github.com>
2024-03-21 21:21:08 +0800

    no4prak3.java

---
+++ no4prak3.java
@@ -0,0 +1,20 @@
+
+import java.util.HashSet;
+
+public class No4 {
+    public static void main(String[] args) {
+        System.out.println("NIM-NIM MAHASISWA");
+        HashSet Nim = new HashSet();
+        Nim.add("D0223007");
+        Nim.add("D0223001");
+        Nim.add("D0223002");
+        Nim.add("D0223003");
+        Nim.add("D0223004");
+        Nim.add("D0223005");
+        Nim.add("D0223006");
+        Nim.add("D0223008");
+        System.out.println(Nim);
+        
+    }
+ 
+}
