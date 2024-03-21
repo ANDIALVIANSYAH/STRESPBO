8ac35b7b719a38b4b84c66ba7f9a73af6f90b37c
ANDIALVIANSYAH <147214020+ANDIALVIANSYAH@users.noreply.github.com>
2024-03-21 21:21:49 +0800

    Create no5prak3.java

---
+++ no5prak3.java
@@ -0,0 +1,20 @@
+
+import java.util.HashMap;
+
+public class No5 {
+
+    public static void main(String[] args) {
+        HashMap<String, String> DataMahasiswa = new HashMap<>();
+        DataMahasiswa.put("D0223007", "AndiAlviansyah.S");
+        DataMahasiswa.put("D0223001", "Nurmadina");
+        DataMahasiswa.put("D0223002", "Arhamullah Kamaruddin");
+        DataMahasiswa.put("D0223003", "Muhammad Faisal");
+        DataMahasiswa.put("D0223004", "Luh Ayu Ariantini");
+        DataMahasiswa.put("D0223005", "Az Zahra");
+        DataMahasiswa.put("D0223006", "Suardi");
+        DataMahasiswa.put("D0223008", "Mutmainnah Thalib");
+        DataMahasiswa.put("D0223009", "Futri Adriana Aksar");
+        System.out.println(DataMahasiswa);
+    }
+
+}
